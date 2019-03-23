a3c53c404c4911669d150d15c2b8444cd57f81e8
Pat <patcat187@gmail.com>
2019-03-23 06:00:38 -0400

    Original Upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var modal = document.getElementById('img-modal');
+var images = document.getElementsByClassName('img-final');
+var modalImg = document.getElementById("imghq");
+var captionText = document.getElementById("caption");
+
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  img.onclick = function(evt) {
+    console.log(evt);
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
